ff960f42f8db4a43e0d9ff43754107d90a14adc3
Ciiro221 <zakariyemohaa2001@gmail.com>
2024-06-29 18:57:21 +1100

    Add files via upload

---
+++ ASSIGNMENT.java
@@ -0,0 +1,30 @@
+package Sem_ch4;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ASSIGNMENT {
+    public static void main(String[] args) {
+        int[] input = {1, 2, 2, 3, 4, 4, 5};
+        int[] result = removeDuplicates(input);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] removeDuplicates(int[] array) {
+        Set<Integer> uniqueElements = new HashSet<>();
+        for (int element : array) {
+            uniqueElements.add(element);
+        }
+
+        int[] uniqueArray = new int[uniqueElements.size()];
+        int index = 0;
+        for (int element : uniqueElements) {
+            uniqueArray[index++] = element;
+        }
+
+        return uniqueArray;
+    }
+    }
+
